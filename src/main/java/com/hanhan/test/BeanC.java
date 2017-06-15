3a2d898168c63481e3db6aa3cc3b9d83b389e09a
hanpenghu <563909408@qq.com>
2017-06-15 22:49:50 +0800

    2017_6_15   weekday(4)   22:49:44

--- src/main/java/com/hanhan/test/BeanC.java
+++ src/main/java/com/hanhan/test/BeanC.java
@@ -1,8 +1,11 @@
 package com.hanhan.test;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Administrator on 2017/6/15 0015.
  */
+@Component
 public class BeanC {
     public void methodC() {
         System.out.println("任务！！------");
