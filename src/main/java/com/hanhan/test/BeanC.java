c98b729573c116facd8949c84a2ceaab52d8dde3
hanpenghu <563909408@qq.com>
2017-06-15 22:27:22 +0800

    2017_6_15   weekday(4)   22:27:15

---
+++ src/main/java/com/hanhan/test/BeanC.java
@@ -0,0 +1,10 @@
+package com.hanhan.test;
+
+/**
+ * Created by Administrator on 2017/6/15 0015.
+ */
+public class BeanC {
+    public void methodC() {
+        System.out.println("任务！！------");
+    }
+}
